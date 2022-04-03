e9ab83360035d8edba7e9154410b36d948f433a4
utopia_developer_learn <utopia_developer@outlook.com>
2022-04-03 21:29:16 +0800

    [add][*] wiki后端框架初始化

---
+++ src/main/java/com/example/wiki_backend/WikiBackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.wiki_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author utopia
+ */
+
+@SpringBootApplication
+public class WikiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiBackendApplication.class, args);
+    }
+
+}
